24ffc1d2b6a460d2003f2c8eb1120d2c43def838
HeltonCarlos <heltonbritopa@gmail.com>
2021-03-26 15:54:41 -0300

    Javascript README
    Javascript - Estacionamento-JS

---
+++ app.js
@@ -0,0 +1,37 @@
+(function(){
+
+    function TabelasCar(car){
+        const row = document.createElement("tr");
+        row.innerHTML+=`
+        <td>${car.name}</td>
+        <td>${car.marca}</td>
+        <td>${car.placa}</td>
+        <td>${car.time}</td>
+        <td><button class="delete";>x</button></td>
+        `
+        document.querySelector("#garage").appendChild(row);
+    }
+
+  document.querySelector('#enviar').addEventListener('click',()=>{  
+    const name=document.getElementById('name').value 
+    const marca=document.getElementById('marca').value 
+    const placa=document.getElementById('placa').value 
+
+    if(!name || !marca || !placa){
+        alert('Por favor, preencha todos os campos*')}
+
+    const car= {name, marca, placa, time:new Date()}
+
+    const garage= localStorage.garage ? JSON.parse(localStorage.garage):[];
+
+    garage.push(car);
+    TabelasCar(car);
+
+    localStorage.garage = JSON.stringify(garage);
+
+    document.getElementById('name').value ="";
+    document.getElementById('marca').value  ="";
+    document.getElementById('placa').value  ="";
+
+  })
+})()
